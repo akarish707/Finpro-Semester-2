0461ea3fdd3d4d2f28ac33c1eb542b8c61ba6986
It's me <74585420+cmp9797@users.noreply.github.com>
2021-06-18 20:18:41 +0700

    Self Laundry

--- finprocoba2.c
+++ finprocoba2.c
@@ -370,43 +370,13 @@ int main(){
 	    
 	    if(option==1)
 		{
-			print_no = 1; //set var global to 1
-			printf("      Choose the order Method: \n");
-		    printf("        [1] Inorder\n");
-			printf("        [2] Preorder\n");
-			printf("        [3] Postorder\n");
-
-			while(1)
-			{
-			    printf("\n      >> Input your choice: ");
-	            scanf("%d", &choice);
-	            
-	            if(choice < 1 || choice > 3)
-	            	printf("        --- Input not valid ---\n");
-	            else
-					break;
-			}
-			
-			switch(choice)
-			{
-				case 1:
-					printf("List data by indorder method\n");
-					break;
-				case 2:
-					printf("List data by preorder method\n");
-					break;
-				case 3:
-					printf("List data by postorder method\n");;
-					break;
-			}
-				
-			printf("\n                                                   --- Laundry List ---\n");
-			printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );	
-		 	printf("    | No. |    ID     |        Customer Name         |   Phone Number   |            Address           |   Heavy   |    Harga    |\n");
-			printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );	
-
+	
 			if(root == NULL)
 			{
+				printf("\n                                                   --- Laundry List ---\n");
+				printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );	
+		 		printf("    | No. |    ID     |        Customer Name         |   Phone Number   |            Address           |   Heavy   |    Harga    |\n");
+				printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );
 				printf("    |                                                                                                                            |\n" );	
 				printf("    |                                                      No Data Available                                                     |\n");
 				printf("    |                                                                                                                            |\n" );	
@@ -414,7 +384,43 @@ int main(){
 			}
 			else
 			{
+				print_no = 1; //set var global to 1
+				printf("      Choose the order Method: \n");
+			    printf("        [1] Inorder\n");
+				printf("        [2] Preorder\n");
+				printf("        [3] Postorder\n");
+	
+				while(1)
+				{
+				    printf("\n      >> Input your choice: ");
+		            scanf("%d", &choice);
+		            
+		            if(choice < 1 || choice > 3)
+		            	printf("        --- Input not valid ---\n");
+		            else
+						break;
+				}
+				
 				switch(choice)
+				{
+					case 1:
+						printf("\n    List data by indorder method\n");
+						break;
+					case 2:
+						printf("\n    List data by preorder method\n");
+						break;
+					case 3:
+						printf("\n    List data by postorder method\n");;
+						break;
+				}
+					
+				
+				printf("\n                                                   --- Laundry List ---\n");
+				printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );	
+		 		printf("    | No. |    ID     |        Customer Name         |   Phone Number   |            Address           |   Heavy   |    Harga    |\n");
+				printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );
+				
+				switch(choice)	
 				{
 					case 1:
 						inorder(root);
@@ -426,9 +432,11 @@ int main(){
 						postorder(root);
 						break;
 				}
-				printf("    |     |           |                              |                  |                              |           |             |\n" );	
-			}
-			printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );	
+				printf("    |     |           |                              |                  |                              |           |             |\n" );				
+			}	
+			printf("    +-----+-----------+------------------------------+------------------+------------------------------+-----------+-------------+\n" );
+
+					
 		}
 		else if(option==2)
 		{
@@ -482,176 +490,140 @@ int main(){
 	    }
 		else if (option==3)
 		{
-			n *temp; 
-			while(1)
+			if(root == NULL)
+				printf("\n    --- No Data Available ---\n");
+			else
 			{
-				getchar();
-				printf("\n   >> Input Order ID L[1-9][0-9]: ");
-				scanf("%c%d", &x, &id);
-				
-				temp = search(root,id);
-				
-				if(x!='L' || (id<10 || id>99))
-	                printf("      --- ID doesn't exist ---\n");
-	            else if (temp != NULL)
-	            	break;
-	            else
+				n *temp; 
+				while(1)
 				{
-					printf("\n      --- ID Is Not Found ---\n");
-					break;
-				}   
-	    	}
-	    	
-            if(temp!=NULL)
-			{
-						
-				int subchoice;
-				
-				printf("\n      Data With ID L%2d Is Found.\n\n", temp->ID);
-				printf("        Customer ID    : %d\n", temp->ID);
-			    printf("        Customer Name  : %s\n", temp->name);
-			    printf("        Contact number : %s\n", temp->number);
-			    printf("        Laundry weight : %d\n", temp->heavy);
-			    printf("        Address        : ");
-			    
-			    if(strcmp(temp->address, "selfpickup") == 0)
-			    	printf("[ Self Pick Up ]\n\n");
-			    else
-			    	printf("%s\n\n", temp->address);
-			    
-			    printf("      You want to Update or Cancel it?\n");
-			    printf("        [1] Update\n");
-				printf("        [2] Cancel\n");
-				
-			    choice = num_validation(3, 2);
-			    
-			    switch(choice)
-			    {
-			    	
-			    	case 1:
-			    	{		
-			    		printf("\n      What do you want to update?\n");
-			    		printf("        [1] Berat\n");
-						printf("        [2] Nama\n");
-						printf("        [3] No\n");
-						printf("        [4] Alamat\n");
-						printf("        [5] Delivery Way\n");
-						printf("        [6] All (except ID)\n\n");
-						
-			    		while(1)
-						{
-						    printf("\n      >> Input your choice: ");
-				            scanf("%d", &subchoice);
-				            
-				            if(subchoice < 1 || subchoice > 6)
-				            	printf("        --- Input not valid ---\n");
-				            else
-								break;
-						}
-						
-						switch(subchoice)
-						{
-							case 1:
-							{
-								temp->heavy = num_validation(1, 2);
-								temp->price = temp->heavy*prc;
-								break;
-							}
-							case 2:
-							{
-								strcpy(temp->name, string_validation(1, 2));
-								break;
-							}	
-							case 3:
-							{
-								strcpy(temp->number, string_validation(2, 2));
-								break;
-							}
-							case 4:
-							{
-								strcpy(temp->address, string_validation(3, 2));
-								break;
-							}
-							case 5:
-							{
-								int ch = num_validation(2, 2);
-			
-								if(ch == 1)
-								   	strcpy(temp->address, "selfpickup");
-								else if(ch == 2)
-									strcpy(temp->address, string_validation(3, 2));
-								break;
-							}
-							case 6:
-							{
-								temp->heavy = num_validation(1, 2);
-								strcpy(temp->name, string_validation(1, 2));
-								strcpy(temp->number, string_validation(2, 2));
-								
-								int ch = num_validation(2, 2);
-			
-								if(ch == 1)
-									strcpy(temp->address, "selfpickup");
-								else if(ch == 2)
-									strcpy(temp->address, string_validation(3, 2));
-								break;
-							}
-						}
-						
-						FILE *fp = fopen("dailydata.txt", "r+");
-						FILE *fpt = fopen("tempdata.txt", "w+");
-						
-						//var to read the file processing
-						int fp_id, fp_berat, fp_harga;
-						char fp_nama[25], fp_no[13], fp_alamat[25];
-				
-						while(fscanf(fp, "L%d#%[^#]#%d#%d#%[^#]#%[^\n]\n", &fp_id, &fp_nama, &fp_berat, &fp_harga, &fp_no, &fp_alamat) != EOF)
-						{	
-//							printf("L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
-
-							if(fp_id == id)
-									fprintf(fpt, "L%d#%s#%d#%d#%s#%s\n", id, temp->name, temp->heavy, temp->price, temp->number, temp->address); 
-								else
-									fprintf(fpt, "L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
-						}
-						
-						fclose(fp);
-						fclose(fpt);
-						
-						remove("dailydata.txt"); //fp
-						rename("tempdata.txt","dailydata.txt");
-						
-						break;
-					}
-					case 2:
+					getchar();
+					printf("\n   >> Input Order ID L[1-9][0-9]: ");
+					scanf("%c%d", &x, &id);
+					
+					temp = search(root,id);
+					
+					if(x!='L' || (id<10 || id>99))
+		                printf("      --- ID doesn't exist ---\n");
+		            else if (temp != NULL)
+		            	break;
+		            else
 					{
-						printf("\n      You Want to Cancel It?\n");
-						printf("        [1] Yes\n");
-						printf("        [2] No\n");
-						
-						subchoice = num_validation(3, 2);
-						
-						if(subchoice == 1)
-						{
-		//					printf("\n\tData With ID L%2d Is Deleted\n",temp->ID);
-							printf("\n    --- Order With ID L%2d Is Canceled ---\n",temp->ID);
-							root = delete(root, id);
+						printf("\n      --- ID Is Not Found ---\n");
+						break;
+					}   
+		    	}
+		    	
+	            if(temp!=NULL)
+				{
 							
+					int subchoice;
+					
+					printf("\n      Data With ID L%2d Is Found.\n\n", temp->ID);
+					printf("        Customer ID    : %d\n", temp->ID);
+				    printf("        Customer Name  : %s\n", temp->name);
+				    printf("        Contact number : %s\n", temp->number);
+				    printf("        Laundry weight : %d\n", temp->heavy);
+				    printf("        Address        : ");
+				    
+				    if(strcmp(temp->address, "selfpickup") == 0)
+				    	printf("[ Self Pick Up ]\n\n");
+				    else
+				    	printf("%s\n\n", temp->address);
+				    
+				    printf("      You want to Update or Cancel it?\n");
+				    printf("        [1] Update\n");
+					printf("        [2] Cancel\n");
+					
+				    choice = num_validation(3, 2);
+				    
+				    switch(choice)
+				    {
+				    	
+				    	case 1:
+				    	{		
+				    		printf("\n      What do you want to update?\n");
+				    		printf("        [1] Berat\n");
+							printf("        [2] Nama\n");
+							printf("        [3] No\n");
+							printf("        [4] Alamat\n");
+							printf("        [5] Delivery Way\n");
+							printf("        [6] All (except ID)\n\n");
 							
-							//file processing
+				    		while(1)
+							{
+							    printf("\n      >> Input your choice: ");
+					            scanf("%d", &subchoice);
+					            
+					            if(subchoice < 1 || subchoice > 6)
+					            	printf("        --- Input not valid ---\n");
+					            else
+									break;
+							}
+							
+							switch(subchoice)
+							{
+								case 1:
+								{
+									temp->heavy = num_validation(1, 2);
+									temp->price = temp->heavy*prc;
+									break;
+								}
+								case 2:
+								{
+									strcpy(temp->name, string_validation(1, 2));
+									break;
+								}	
+								case 3:
+								{
+									strcpy(temp->number, string_validation(2, 2));
+									break;
+								}
+								case 4:
+								{
+									strcpy(temp->address, string_validation(3, 2));
+									break;
+								}
+								case 5:
+								{
+									int ch = num_validation(2, 2);
+				
+									if(ch == 1)
+									   	strcpy(temp->address, "selfpickup");
+									else if(ch == 2)
+										strcpy(temp->address, string_validation(3, 2));
+									break;
+								}
+								case 6:
+								{
+									temp->heavy = num_validation(1, 2);
+									strcpy(temp->name, string_validation(1, 2));
+									strcpy(temp->number, string_validation(2, 2));
+									
+									int ch = num_validation(2, 2);
+				
+									if(ch == 1)
+										strcpy(temp->address, "selfpickup");
+									else if(ch == 2)
+										strcpy(temp->address, string_validation(3, 2));
+									break;
+								}
+							}
+							
 							FILE *fp = fopen("dailydata.txt", "r+");
 							FILE *fpt = fopen("tempdata.txt", "w+");
 							
 							//var to read the file processing
 							int fp_id, fp_berat, fp_harga;
 							char fp_nama[25], fp_no[13], fp_alamat[25];
-							
+					
 							while(fscanf(fp, "L%d#%[^#]#%d#%d#%[^#]#%[^\n]\n", &fp_id, &fp_nama, &fp_berat, &fp_harga, &fp_no, &fp_alamat) != EOF)
-							{
-//								printf("%d: L%d %s %d %d %s %s\n", id, fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
-		
+							{	
+								//DEBUG
+	//							printf("L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
+	
 								if(fp_id == id)
-									continue;
+										fprintf(fpt, "L%d#%s#%d#%d#%s#%s\n", id, temp->name, temp->heavy, temp->price, temp->number, temp->address); 
 									else
 										fprintf(fpt, "L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
 							}
@@ -661,53 +633,100 @@ int main(){
 							
 							remove("dailydata.txt"); //fp
 							rename("tempdata.txt","dailydata.txt");
+							
+							break;
+						}
+						case 2:
+						{
+							printf("\n      You Want to Cancel It?\n");
+							printf("        [1] Yes\n");
+							printf("        [2] No\n");
+							
+							subchoice = num_validation(3, 2);
+							
+							if(subchoice == 1)
+							{
+								printf("\n    --- Order With ID L%2d Is Canceled ---\n",temp->ID);
+								root = delete(root, id);
+															
+								//file processing
+								FILE *fp = fopen("dailydata.txt", "r+");
+								FILE *fpt = fopen("tempdata.txt", "w+");
+								
+								//var to read the file processing
+								int fp_id, fp_berat, fp_harga;
+								char fp_nama[25], fp_no[13], fp_alamat[25];
+								
+								while(fscanf(fp, "L%d#%[^#]#%d#%d#%[^#]#%[^\n]\n", &fp_id, &fp_nama, &fp_berat, &fp_harga, &fp_no, &fp_alamat) != EOF)
+								{
+									//DEBUG
+	//								printf("%d: L%d %s %d %d %s %s\n", id, fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
+			
+									if(fp_id == id)
+										continue;
+										else
+											fprintf(fpt, "L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
+								}
+								
+								fclose(fp);
+								fclose(fpt);
+								
+								remove("dailydata.txt"); //fp
+								rename("tempdata.txt","dailydata.txt");
+							}
 						}
 					}
 				}
-
-				
-				
-			
+		    
 			}
-	    }
+		}
 	    
 		else if (option==4)
 		{
-			while(1)
-			{
-				getchar();
-				printf("    >> Input Order ID L[1-9][0-9]: "); 
-          		scanf("%c%d",&x, &id); 
-          		
-          		n*temp = search(root,id);
-          		
-				if(x!='L' || (id<10 || id>99))
-			        printf("        --- ID doesn't exist ---\n\n");
-		     	else if(temp == NULL)
-		   	      	printf("        --- Member ID is not found ---\n");
-		    	else if(temp!=NULL)
+			if(root == NULL)
+				printf("\n    --- No Data Available ---\n");
+			else
+			{	
+				while(1)
 				{
-//              		printf("\n    L%2d - %-14s  %2d Kg %-13s %-23s \n\tHas Been Sucessfully Taken\n", temp->ID, temp->name, temp->heavy, temp->number, temp->address);
-              		printf("\n    Order with\n\n");
-				    printf("      Customer ID    : %d\n", temp->ID);
-				    printf("      Customer Name  : %s\n", temp->name);
-				    printf("      Contact number : %s\n", temp->number);
-				    printf("      Laundry weight : %d\n", temp->heavy);
-				    printf("      Address        : ");
-				    if(strcmp(temp->address, "   ( Self Pick Up )      ") == 0)
-				    	printf("[ Self Pick Up ]\n\n");
-				    else
-				    	printf("%s\n\n", temp->address);
-				    	
-				    printf("\n    Has Been Sucessfully Taken\n");	
-					root = delete(root,id);
-				    break;
-		    	}
-			}
-	    }
-	    else if (option==5){
+					getchar();
+					printf("    >> Input Order ID L[1-9][0-9]: "); 
+	          		scanf("%c%d",&x, &id); 
+	          		
+	          		n*temp = search(root,id);
+	          		
+					if(x!='L' || (id<10 || id>99))
+				        printf("        --- ID doesn't exist ---\n\n");
+			     	else if(temp == NULL)
+			   	      	printf("        --- Member ID is not found ---\n");
+			    	else if(temp!=NULL)
+					{
+	//              		printf("\n    L%2d - %-14s  %2d Kg %-13s %-23s \n\tHas Been Sucessfully Taken\n", temp->ID, temp->name, temp->heavy, temp->number, temp->address);
+	              		printf("\n    Order with\n\n");
+					    printf("      Customer ID    : %d\n", temp->ID);
+					    printf("      Customer Name  : %s\n", temp->name);
+					    printf("      Contact number : %s\n", temp->number);
+					    printf("      Laundry weight : %d\n", temp->heavy);
+					    printf("      Address        : ");
+					    if(strcmp(temp->address, "   ( Self Pick Up )      ") == 0)
+					    	printf("[ Self Pick Up ]\n\n");
+					    else
+					    	printf("%s\n\n", temp->address);
+					    	
+					    printf("\n    Has Been Sucessfully Taken\n");	
+						root = delete(root,id);
+					    break;
+			    	}
+				}
+		    }
+		}
+	    else if (option==5)
+		{
 	    	
-	    	FILE *fp = fopen("dailydata.txt", "r+");
+			time_t t = time(NULL);
+			struct tm tm = *localtime(&t);
+
+			FILE *fp = fopen("dailydata.txt", "r+");
 			FILE *fpt = fopen("laundries.txt", "a+");
 			
 			//var to read the file processing
@@ -716,20 +735,20 @@ int main(){
 	
 			while(fscanf(fp, "L%d#%[^#]#%d#%d#%[^#]#%[^\n]\n", &fp_id, &fp_nama, &fp_berat, &fp_harga, &fp_no, &fp_alamat) != EOF)
 			{	
-				printf("L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
-
-//				if(fp_id == id)
-//						fprintf(fpt, "L%d#%s#%d#%d#%s#%s\n", id, temp->name, temp->heavy, temp->price, temp->number, temp->address); 
-//					else
-						fprintf(fpt, "L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
+				//DEBUG
+//					printf("L%d#%s#%d#%d#%s#%s\n", fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); fpt, "L%d#%s#%d#%d#%s#%s\n", id, temp->name, temp->heavy, temp->price, temp->number, temp->address); 
+				fprintf(fpt, "%d/%02d/%02d#L%d#%s#%d#%d#%s#%s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, fp_id, fp_nama, fp_berat, fp_harga, fp_no, fp_alamat); 
 			}
 			
 			fclose(fp);
 			fclose(fpt);
 			
 			remove("dailydata.txt"); //fp
-	    	
+			
+			printf("\n    Thank You For Using The Application ^_^\n");
+
 			exit(1);
+		    
 	    }
 	    else
 	   		printf("\n    Sorry, your input invalid.\n");
@@ -739,7 +758,6 @@ int main(){
         system("CLEAR || CLS");
   }
   
-  	printf("\n Thank You For Using The Application ^_^\n");
   	return 0;
   
 }
